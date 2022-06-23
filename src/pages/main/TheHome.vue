d9075a3ae7d9c41afd1510dcccfb2a2a20803462
Kiho <kiho2735@icloud.com>
2022-06-23 11:38:52 -0400

    Set basis home set up

--- src/pages/main/TheHome.vue
+++ src/pages/main/TheHome.vue
@@ -1,7 +1,24 @@
-<template>Home</template>
+<template>
+  <div class="container">
+    <div class="welcome">
+      <div class="welcome__msg">
+        <h2 class="welcome__title">Welcome!</h2>
+        <p class="welcome__introduce">
+          This is a personal blog. You can explore many interesting types of
+          blog posts.
+        </p>
+        <router-link to="#">Explore blogs</router-link>
+      </div>
+      <div class="welcome__img">
+        <img src="../../assets/welcomeImage.png" alt="welcomeImage" />
+      </div>
+    </div>
+    <div class="recent__blog"></div>
+  </div>
+</template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style lang="scss" scoped></style>
