834397e721f85cb4d6939d876a926fb4f8eaa2ce
Kiho <kiho2735@icloud.com>
2022-06-28 09:27:03 -0400

    Add route for NotFound

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,1 +1,24 @@
-<template>Page Not Found.</template>
+<template>
+  <section>
+    <h2>Page not found</h2>
+    <p>
+      Go back to <router-link :to="{ name: 'Home' }">Home</router-link> Page.
+    </p>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+section {
+  height: 80vh;
+  padding: 6rem 8rem;
+  font-size: 2rem;
+}
+
+a {
+  text-decoration: underline;
+
+  &:hover {
+    color: red;
+  }
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -2,12 +2,14 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import TheHome from "./pages/TheHome.vue";
 import UserAuth from "./pages/UserAuth.vue";
+import NotFound from "./pages/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: "/home", name: "Home", component: TheHome },
+    { path: "/", name: "Home", component: TheHome },
     { path: "/login", component: UserAuth },
+    { path: "/:notFound(.*)", component: NotFound },
   ],
 });
 
