98649727220ccbf7b2f82d0d71dea554e5440f86
Kiho <kiho2735@icloud.com>
2022-06-30 11:01:51 -0400

    Change style for login page

--- src/pages/UserAuth.vue
+++ src/pages/UserAuth.vue
@@ -5,44 +5,49 @@
   <base-dialog :show="isLoading" title="Authenticating...">
     Loading...
   </base-dialog>
+
   <div class="form-wrapper">
-    <div class="form-wrapper__container">
+    <div class="title">
+      <h1>Calm</h1>
+    </div>
+
+    <form @submit.prevent="login" class="form-wrapper__login">
       <div class="form-wrapper__title">
-        <h1>Calm</h1>
+        <h2>Sign In</h2>
       </div>
 
-      <form @submit.prevent="login" class="form-wrapper__login">
-        <div class="form-control">
-          <font-awesome-icon :icon="['fa', 'user']" class="icon" />
-          <input
-            type="email"
-            id="id"
-            v-model.trim="userId.val"
-            @blur="validateId"
-            placeholder="Email"
-          />
-          <p v-if="userId.isEmpty">Please enter email.</p>
-          <p v-else-if="userId.isValid">Please enter valid email.</p>
-        </div>
-        <div class="form-control">
-          <font-awesome-icon :icon="['fa', 'lock']" class="icon" />
-          <input
-            type="password"
-            id="password"
-            v-model.trim="password.val"
-            @blur="validatePassword"
-            placeholder="Password"
-          />
-          <p v-if="password.isEmpty">Please enter password.</p>
-        </div>
-        <p class="form-wrapper__register">
-          Forgot your <router-link to="#">username</router-link> or
-          <router-link to="#">password</router-link>? |
-          <router-link to="#">Register</router-link>
-        </p>
-        <button class="submit">Sign In</button>
-      </form>
-    </div>
+      <div class="form-control">
+        <font-awesome-icon :icon="['fa', 'user']" class="icon" />
+        <input
+          type="email"
+          id="id"
+          v-model.trim="userId.val"
+          @blur="validateId"
+          placeholder="Email"
+          :class="redBorderColorId"
+        />
+        <p v-if="userId.isEmpty">Please enter email.</p>
+        <p v-else-if="userId.isValid">Please enter valid email.</p>
+      </div>
+      <div class="form-control">
+        <font-awesome-icon :icon="['fa', 'lock']" class="icon" />
+        <input
+          type="password"
+          id="password"
+          v-model.trim="password.val"
+          @blur="validatePassword"
+          placeholder="Password"
+          :class="redBorderColorPassword"
+        />
+        <p v-if="password.isEmpty">Please enter password.</p>
+      </div>
+      <p class="form-wrapper__register">
+        Forgot your <router-link to="#">username</router-link> or
+        <router-link to="#">password</router-link>? |
+        <router-link to="#">Register</router-link>
+      </p>
+      <button class="submit-btn">Sign In</button>
+    </form>
   </div>
 </template>
 
@@ -62,6 +67,14 @@ export default {
       isLoading: false,
       error: null,
     };
+  },
+  computed: {
+    redBorderColorId() {
+      return { redBorder: this.userId.isEmpty || this.userId.isValid };
+    },
+    redBorderColorPassword() {
+      return { redBorder: this.password.isEmpty };
+    },
   },
   methods: {
     async login() {
@@ -118,29 +131,39 @@ export default {
   align-items: center;
   height: 100vh;
 
-  &__container {
+  .title {
+    font-size: 1.5rem;
+    margin-bottom: 1rem;
+  }
+
+  &__title {
+    width: 100%;
+    margin-bottom: 1rem;
+  }
+
+  &__login {
     border: 1px solid rgb(190, 190, 190);
     border-radius: 15px;
-    position: relative;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
     font-size: 1.5rem;
-    padding: 3rem;
-  }
-
-  &__title {
-    padding: 0 1rem;
-    position: absolute;
-    top: -40px;
-    background-color: #fff;
+    padding: 3rem 2rem 4rem;
   }
 
   .form-control {
     position: relative;
     margin-bottom: 1rem;
-    width: 350px;
+    width: 300px;
+
+    @media (min-width: 700px) {
+      width: 350px;
+    }
+
+    .redBorder {
+      border: 1px solid red;
+    }
 
     .icon {
       position: absolute;
@@ -157,7 +180,7 @@ export default {
     }
 
     p {
-      margin-top: 0.5rem;
+      margin-top: 0.2rem;
       font-size: 1rem;
       color: rgb(253, 60, 60);
     }
@@ -179,7 +202,7 @@ export default {
     }
   }
 
-  .submit {
+  .submit-btn {
     width: 100%;
     padding: 0.5rem 1rem;
     margin-top: 1rem;
